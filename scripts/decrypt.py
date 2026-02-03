80933661924ac06f3b700d99eb6ea8adf0654dcd
Alexis Rangel <alexiscash97@rocketmail.com>
2026-02-02 20:34:48 -0600

    added encrypt file and venv

--- scripts/decrypt.py
+++ scripts/decrypt.py
@@ -4,30 +4,36 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 import json
 
-CSHARP_HEADER = [0, 1, 0, 0, 0, 255, 255, 255,
-                 255, 1, 0, 0, 0, 0, 0, 0, 0, 6, 1, 0, 0, 0]
-AES_KEY_STRING = "UKu52ePUBwetZ9wNX88o54dnfKRu0T1l"
+raw = Path('saves/user3.dat').read_bytes()
 
-raw = Path('saves/user2.dat').read_bytes()
 
-without_header = raw[len(CSHARP_HEADER):len(raw)-1]
+def decrypt(payload: bytes) -> str:  # JSON
+    CSHARP_HEADER = [0, 1, 0, 0, 0, 255, 255, 255,
+                     255, 1, 0, 0, 0, 0, 0, 0, 0, 6, 1, 0, 0, 0]
+    AES_KEY_STRING = "UKu52ePUBwetZ9wNX88o54dnfKRu0T1l"
 
-len_count = 0
-for i in range(5):
-    len_count += 1
-    if (without_header[i] & 0x80) == 0:
-        break
+    without_header = raw[len(CSHARP_HEADER):len(raw)-1]
 
-payload = without_header[len_count:]
+    len_count = 0
+    for i in range(5):
+        len_count += 1
+        if (without_header[i] & 0x80) == 0:
+            break
 
-b64_encrypted = base64.b64decode(payload)
-mode = AES.MODE_ECB
-cipher = AES.new(AES_KEY_STRING.encode(), mode)
+    payload = without_header[len_count:]
 
-decoded = cipher.decrypt(b64_encrypted)
-unpadded = unpad(decoded, AES.block_size)
+    b64_encrypted = base64.b64decode(payload)
+    cipher = AES.new(AES_KEY_STRING.encode(), AES.MODE_ECB)
 
-pretty = json.loads(unpadded)
+    decoded = cipher.decrypt(b64_encrypted)
+    unpadded = unpad(decoded, AES.block_size)
 
-with open('saves/user2.json', 'w') as f:
-    json.dump(pretty, f, ensure_ascii=False)
+    return json.loads(unpadded)
+
+
+def dump_to_json(pretty: str) -> None:
+    with open('saves/user3.json', 'w') as f:
+        json.dump(pretty, f, ensure_ascii=False)
+
+
+dump_to_json(decrypt(raw))
---
+++ scripts/encrypt.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+import base64
+
+CSHARP_HEADER = bytes([0, 1, 0, 0, 0, 255, 255, 255,
+                       255, 1, 0, 0, 0, 0, 0, 0, 0, 6, 1, 0, 0, 0])
+AES_KEY_STRING = "UKu52ePUBwetZ9wNX88o54dnfKRu0T1l"
+
+
+def encrypt(json_bytes: bytes) -> bytes:
+    cipher = AES.new(AES_KEY_STRING.encode(), AES.MODE_ECB)
+
+    encrypted = cipher.encrypt(pad(json_bytes, AES.block_size))
+
+    b64_bytes = base64.b64encode(encrypted)
+
+    out = bytearray()
+
+    n = len(b64_bytes)
+    while True:
+        byte = n & 0x7f
+        n >>= 7
+        if n:
+            out.append(byte | 0x80)
+        else:
+            out.append(byte)
+            break
+
+    len_bytes = bytes(out)
+
+    return (
+        CSHARP_HEADER +
+        len_bytes +
+        b64_bytes +
+        b"\x0B"
+    )
+
+
+def save_to_dat_file(final_bytes: bytes) -> None:
+    Path("saves/user3.dat").write_bytes(final_bytes)
